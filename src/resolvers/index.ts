ba70436fbf3becefff502d8f469262125a58b2b9
Jordan Cotter <jrdn.cttr@gmail.com>
2022-08-30 09:52:28 -0500

    Update index.ts

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -3,11 +3,7 @@ import { v4 as uuid } from 'uuid'
 
 export default {
   DateTime: GQLDate,
-  Query: {
-    id: () => `123`
-  },
-  Mutation: {
-    createId: () => uuid()
-  },
+  Query: {},
+  Mutation: {},
 }
 
